4be57a391eca1d27a234b35fd6d8bc341deb86da
Trop <john-outsider@hotmail.com>
2016-06-12 16:08:38 +0300

    Added Citizen and CitizenRegistry classes.

---
+++ Citizens/Citizen.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Citizens
+{
+    public class Citizen : ICitizen
+    {
+        private string firstName;
+        private string lastName;
+        private Gender gender;
+        private DateTime birthDate;
+        private string vatId;
+
+        public string FirstName
+        {
+            get { return firstName; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+        }
+
+        public Gender Gender
+        {
+            get { return gender; }
+        }
+
+        public DateTime BirthDate
+        {
+            get { return birthDate; }
+        }
+
+        public string VatId
+        {
+            get
+            {
+                return vatId;
+            }
+            set
+            {
+                vatId = value;
+            }
+        }
+
+        public Citizen(string firstName, string lastName, DateTime birthDate, Gender gender)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.birthDate = birthDate;
+            this.gender = gender;
+        }
+    }
+
+
+}
---
+++ Citizens/CitizenRegistry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Citizens
+{
+    public class CitizenRegistry : ICitizenRegistry
+    {
+        public void Register(ICitizen citizen)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICitizen this[string id]
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string Stats()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
